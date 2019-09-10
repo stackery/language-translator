e4c39f2537d1b95568fb1ed2074519c820079cb0
Farrah Campbell <farrahcampbell@Garretts-MBP.localdomain>
2019-09-10 16:02:23 -0700

    start get bucket contents

--- src/TranslateFunction/index.js
+++ src/TranslateFunction/index.js
@@ -1,6 +1,28 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const translate = new AWS.Translate();
+
+
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
+  console.log(event.Records[0].s3.bucket.name);
+  console.log(event.Records[0].s3.object.key);
 
+  const bucketName = event.Records[0].s3.bucket.name;
+  const Key = event.Records[0].s3.object.key;
+
+  try {
+    const bucketContents = await s3.getObject({
+      Bucket: bucketName,
+      Key
+    }).promise();
+    
+    console.log(bucketContents);
+    
+  } catch (error) {
+    console.log(error);
+
+  }
   return {};
 };
