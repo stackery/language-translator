38423d19d88f12265217c9ebef94d7b5aeb8995d
danielle <danielle@stackery.io>
2019-09-18 13:17:08 -0700

    Minor cleanup

--- src/TranslateFunction/index.js
+++ src/TranslateFunction/index.js
@@ -2,18 +2,18 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const translate = new AWS.Translate();
 
-exports.handler = async (event, context) => {
+exports.handler = async event => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
   console.log(event.Records[0].s3.bucket.name);
   console.log(event.Records[0].s3.object.key);
 
-  const bucketName = event.Records[0].s3.bucket.name;
+  const originalBucketName = event.Records[0].s3.bucket.name;
   const Key = event.Records[0].s3.object.key;
 
   try {
     const bucketContents = await s3.getObject({
-      Bucket: bucketName,
+      Bucket: originalBucketName,
       Key
     }).promise();
 
@@ -29,16 +29,15 @@ exports.handler = async (event, context) => {
     const translatedText = await translate.translateText(TranslateParams).promise();
     console.log(translatedText);
 
-    const PutObjectParams = {
+    const putObjectParams = {
       Bucket: process.env.BUCKET_NAME,
       Key,
       Body: translatedText.TranslatedText
     };
-    const PutObjectResult = await s3.putObject(PutObjectParams).promise();
-    console.log(PutObjectResult);
+
+    const putObjectResult = await s3.putObject(putObjectParams).promise();
+    console.log(putObjectResult);
   } catch (error) {
     console.log(error);
   }
-
-  return {};
 };
