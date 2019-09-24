ab68b7cf60b03ef7b358383a65b0e00a6210a761
danielle <danielle@stackery.io>
2019-09-24 15:03:55 -0700

    Target language dynamic

--- src/frontend-code/src/components/App.js
+++ src/frontend-code/src/components/App.js
@@ -12,16 +12,25 @@ const s3 = new AWS.S3({ region: 'us-west-2' });
 class App extends Component {
   constructor () {
     super();
-    this.file = null;
-    this.state = {};
+    this.file = '';
+    this.state = {
+      targetLanguage: 'ar'
+    };
 
     this.handleFileChange = this.handleFileChange.bind(this);
+    this.handleTargetLanguageChange = this.handleTargetLanguageChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleFileChange (event) {
     this.file = event.target.files[0];
     console.log(this.file);
+  }
+
+  handleTargetLanguageChange (event) {
+    this.setState({
+      targetLanguage: event.target.value
+    });
   }
 
   async handleSubmit (event) {
@@ -42,10 +51,13 @@ class App extends Component {
       const result = await s3.putObject({
         Body: this.file,
         Bucket: 'language-translator-development-originalf-053662045684',
-        Key: `en/ru/${this.file.name}`,
+        Key: `en/${this.state.targetLanguage}/${this.file.name}`,
         ContentType: 'text/plain'
       }).promise();
-      this.file = null;
+      this.file = '';
+      this.setState({
+        targetLanguage: 'ar'
+      });
       console.log('FINISH UPLOAD ', result);
     } catch (error) {
       console.log('AN ERROR OCURRED');
@@ -54,10 +66,14 @@ class App extends Component {
   }
 
   render () {
+    console.log(this.state);
     return (
       <div className='container'>
         <h1>Language Translator</h1>
         <Form
+          file={this.file}
+          targetLanguage={this.state.targetLanguage}
+          onTargetLanguageChange={this.handleTargetLanguageChange}
           onFileChange={this.handleFileChange}
           onSubmit={this.handleSubmit}
         />
--- src/frontend-code/src/components/Form.js
+++ src/frontend-code/src/components/Form.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import '../styles/Form.css';
 
 function Form (props) {
+  const options = (
+    <Fragment>
+      <option value='ar'>Arabic</option>
+      <option value='zh'>Chinese (Simplified)</option>
+      <option value='zh-TW'>Chinese (Traditional)</option>
+      <option value='cs'>Czech</option>
+      <option value='da'>Danish</option>
+      <option value='nl'>Dutch</option>
+      <option value='en'>English</option>
+      <option value='fi'>Finnish</option>
+      <option value='fr'>French</option>
+      <option value='de'>German</option>
+      <option value='he'>Hebrew</option>
+      <option value='hi'>Hindi</option>
+      <option value='id'>Indonesian</option>
+      <option value='it'>Italian</option>
+      <option value='ja'>Japanese</option>
+      <option value='ko'>Korean</option>
+      <option value='ms'>Malay</option>
+      <option value='no'>Norwegian</option>
+      <option value='fa'>Persian</option>
+      <option value='pl'>Polish</option>
+      <option value='pt'>Portuguese</option>
+      <option value='ru'>Russian</option>
+      <option value='es'>Spanish</option>
+      <option value='sv'>Swedish</option>
+      <option value='tr'>Turkish</option>
+    </Fragment>
+  );
+
   return (
     <div className='form-container'>
       <form onSubmit={props.onSubmit}>
-        <input onChange={props.onFileChange} type='file' />
+        <select name='targetLanguage' value={props.targetLanguage} onChange={props.onTargetLanguageChange}>
+          { options }
+        </select>
+        <input onChange={props.onFileChange} type='file' value={props.file} />
         <button type='submit' className='submitButton'>Submit</button>
       </form>
     </div>
