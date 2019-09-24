2f1a57be0c421f735221f32897d1e3c2f8d9b47d
danielle <danielle@stackery.io>
2019-09-24 09:18:29 -0700

    Add dynamoDB table and make language source/target dynamic

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ This is a tool that will translate text into another language.
 
 ## Directions to Use
 1. Deploy the stack. It is recommended to use [Stackery](https://www.stackery.io/)
-2. Upload a text file to the `OriginalFiles` s3 bucket in the AWS console
+2. Upload a text file to the `OriginalFiles` s3 bucket in the AWS console. Should be in format `SourceLanguageCode/TargetLanguageCode/filename.txt`.
 3. The translated text will appear in the `TranslatedFiles` s3 bucket
 
-## Notes 
+## Notes
 - `translateText` has a [limit](https://docs.aws.amazon.com/translate/latest/dg/what-is-limits.html) - ensure your filesize does not exceed this
 - edit the `SourceLanguageCode` and `TargetLanguageCode` in `src/TranslateFunction/index.js` if you'd like to do a combo aside from English to Spanish. Supported language codes are [here](https://docs.aws.amazon.com/translate/latest/dg/how-it-works.html#how-it-works-language-codes).
--- src/TranslateFunction/index.js
+++ src/TranslateFunction/index.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const translate = new AWS.Translate();
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async event => {
   // Log the event argument for debugging and for use in local development.
@@ -10,6 +11,8 @@ exports.handler = async event => {
 
   const originalBucketName = event.Records[0].s3.bucket.name;
   const Key = event.Records[0].s3.object.key;
+  const SourceLanguageCode = Key.split('/')[0];
+  const TargetLanguageCode = Key.split('/')[1];
 
   try {
     const bucketContents = await s3.getObject({
@@ -21,8 +24,8 @@ exports.handler = async event => {
     console.log(Text);
 
     const TranslateParams = {
-      SourceLanguageCode: 'en',
-      TargetLanguageCode: 'es',
+      SourceLanguageCode,
+      TargetLanguageCode,
       Text
     };
 
@@ -37,6 +40,20 @@ exports.handler = async event => {
 
     const putObjectResult = await s3.putObject(putObjectParams).promise();
     console.log(putObjectResult);
+
+    const putDynamoParams = {
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        Key,
+        SourceLanguageCode,
+        TargetLanguageCode,
+        OriginalText: Text,
+        TranslatedText: translatedText.TranslatedText
+      }
+    };
+
+    const putDynamoResult = await dynamodb.put(putDynamoParams).promise();
+    console.log(putDynamoResult);
   } catch (error) {
     console.log(error);
   }
